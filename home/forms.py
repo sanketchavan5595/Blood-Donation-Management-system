93ba4b4b4cddf54e55a65970ef1ffc62a9b03d61
Sanket Chavan <sanketchavan5595@gmail.com>
2021-10-04 16:41:52 +0530

    added select options to django forms

--- home/forms.py
+++ home/forms.py
@@ -1,12 +1,32 @@
 from django import forms
-from django.forms import fields 
+from django.forms import fields, widgets 
 from .models import Patient, Bloodbank, Hospital, Donor, Blood
 
 class PatientForm(forms.ModelForm):
     class Meta:
         model = Patient
 
+        genderChoices = (
+            ("Male", "M"),
+            ("Female", "F"),
+        )
+
+        bloodgroupChoices = (
+            ("O+", "O+" ),
+            ("O-", "O-" ),
+            ("A+", "A+" ),
+            ("A-", "A-" ),
+            ("B+", "B+" ),
+            ("B-", "B-" ),
+            ("AB+", "AB+" ),
+            ("AB-", "AB-" ),
+        )
+
         fields = "__all__"
+        widgets = {
+            'gender': forms.Select(choices=genderChoices),
+            'pbloodtype': forms.Select(choices=bloodgroupChoices),
+        }
 
 class BloodbankForm(forms.ModelForm):
     class Meta:
@@ -26,8 +46,36 @@ class DonorForm(forms.ModelForm):
 
         fields = "__all__"
 
+        genderChoices = (
+            ("Male", "M"),
+            ("Female", "F"),
+        )
+
+        widgets = {
+            'gender': forms.Select(choices=genderChoices),
+            'age': forms.NumberInput(attrs={'min':18,'max': '60','type': 'number'})
+        }
+
+
 class BloodForm(forms.ModelForm):
     class Meta:
         model = Blood
 
         fields = "__all__"
+
+        bloodgroupChoices = (
+            ("O+", "O+" ),
+            ("O-", "O-" ),
+            ("A+", "A+" ),
+            ("A-", "A-" ),
+            ("B+", "B+" ),
+            ("B-", "B-" ),
+            ("AB+", "AB+" ),
+            ("AB-", "AB-" ),
+        )
+
+        widgets = {
+            'bloodtype': forms.Select(choices=bloodgroupChoices)
+        }
+
+
